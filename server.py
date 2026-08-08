1b4fcb965566bf9985715bbcbd03d7e837d849ab
Arvind Kayande <avikayande0007@gmail.com>
2026-08-08 17:13:29 +0530

    Update server.py

--- server.py
+++ server.py
@@ -178,9 +178,7 @@ def upload_base64_to_freeimage(base64_data, filename):
 
             "source": base64_data,
 
-            "format": "json",
-
-            "album_id": "HKFzdJ"
+            "format": "json"
         }
 
 
